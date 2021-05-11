5cd2a931341e8e1b19afb756e900c7664b1564e6
pixus <pix_us@mail.ru>
2021-05-11 17:28:48 -0400

    Initial Commit

---
+++ Dicee SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee SwiftUI
+//
+//  Created by locussigilli on 5/11/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
